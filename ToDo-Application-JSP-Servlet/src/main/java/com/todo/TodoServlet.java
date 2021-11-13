3998a2a86c72ef940c921d995d376db303e24c72
pratibhapw23 <pratibha_wabale@persistent.com>
2021-11-13 14:31:37 +0530

    Created New Servlet

---
+++ ToDo-Application-JSP-Servlet/src/main/java/com/todo/TodoServlet.java
@@ -0,0 +1,21 @@
+package com.todo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/todo.do")
+public class TodoServlet extends HttpServlet{
+
+	TodoService todoservice=new TodoService();
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("todos", todoservice.getTodos());
+		request.getRequestDispatcher("/WEB-INF/views/todolistpage.jsp").forward(request, response);
+	}
+}
